896067f35937931697201da067ad9cfb02ba79ad
18811126599 <m18811126599@163.com>
2016-10-08 10:42:25 +0800

    添加注释说明

--- core/config/database.php
+++ core/config/database.php
@@ -1,10 +1,10 @@
 <?php 
 	return array(
-		'database_type' => 'mysql',
-	    'database_name' => 'pjds',
-	    'server' => 'localhost',
-	    'username' => 'root',
-	    'password' => 'root',
-	    'charset' => 'utf8'	
+		'database_type' => 'mysql', //数据库类型
+	    'database_name' => 'app', //数据库名称
+	    'server' => 'localhost', //地址
+	    'username' => 'root', //用户名
+	    'password' => 'root', //用户密码
+	    'charset' => 'utf8'	// 字符集
 	);
 ?>
